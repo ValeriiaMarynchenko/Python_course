e085a62c6f08b4e188b778cb941b2d324c861ad1
DOOMASTER73 <106273461+DOOMASTER73@users.noreply.github.com>
2023-11-01 01:19:36 +0200

    Add files via upload

---
+++ if_elif_else_1_exercise_answer.py
@@ -0,0 +1,10 @@
+my_list = [316, 413, 374, 124, 474, 295, 69, 172, 500, 198, 170, 318]
+my_list1 = [350, 308, 44, 443, 500, 409, 54, 328, 20, 316, 413, 374, 124, 474, 295, 69, 172, 500, 198, 170, 318, 449, 119, 435, 179, 429, 113, 453, 420, 80, 292, 322, 143]
+a = 124
+if a in my_list:
+    print("a in my_list")
+elif a in my_list1:
+    print("a not in my_list,  but a in my_list1")
+else:
+    print("a doesn't include in both lists")
+
